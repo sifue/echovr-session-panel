4c7a10ecbb5fcbb262d2ee0e5aeb15b20f0842fc
sifue <yoshimura@soichiro.org>
2021-03-18 20:12:30 +0900

    Format

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
@@ -21,11 +21,10 @@ app.whenReady().then(() => {
       createWindow();
     }
   });
-
 });
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
