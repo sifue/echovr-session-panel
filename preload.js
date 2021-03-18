c817b9ed638a487103773ef3201735e9cbb883ec
sifue <yoshimura@soichiro.org>
2021-03-18 23:51:27 +0900

    finished version 1

--- preload.js
+++ preload.js
@@ -6,6 +6,14 @@ window.addEventListener('DOMContentLoaded', () => {
   }, 1000);
 });
 
+function zeroPaddingString(n) {
+  if (n < 10) {
+    return '0' + n;
+  } else {
+    return n;
+  }
+}
+
 /**
  * render HTML with session API Data
  * @param {*} sessionData see https://github.com/Ajedi32/echovr_api_docs
@@ -16,33 +24,73 @@ function renderHTML(sessionData) {
     if (element) element.innerText = text;
   };
 
-  console.log(sessionData);
+  const sessionid = sessionData.sessionid;
+  replaceText('sessionid', sessionid);
 
-  const isPlaying = sessionData.game_status === 'playing';
+  const game_status = sessionData.game_status;
+
+  if (!game_status) {
+    return;
+  }
+
   const game_clock_display = sessionData.game_clock_display.split('.')[0];
-  const orange_points = sessionData.orange_points;
-  const blue_points = sessionData.blue_points;
-  const orange_team = sessionData.teams[1].players.map((p) => { return {
-    level: p.level,
-    name: p.name,
-    point: p.stats.points,
-    assists: p.stats.assists,
-    saves: p.stats.saves,
-    stuns: p.stats.stuns,
-    ping: p.ping
-  };});
-  const blue_team = sessionData.teams[0].players.map((p) => { return {
-    level: p.level,
-    name: p.name,
-    point: p.stats.points,
-    assists: p.stats.assists,
-    saves: p.stats.saves,
-    stuns: p.stats.stuns,
-    ping: p.ping
-  };});
+  replaceText('game-clock-display', game_clock_display);
 
-  replaceText(
-    'game-clock-display',
-    sessionData.game_clock_display.split('.')[0]
-  );
+  const orange_points = sessionData.orange_points;
+  replaceText('orange-points', zeroPaddingString(orange_points));
+
+  const blue_points = sessionData.blue_points;
+  replaceText('blue-points', zeroPaddingString(blue_points));
+
+  const orange_team = sessionData.teams[1].players.map((p) => {
+    return {
+      level: p.level,
+      name: p.name,
+      point: p.stats.points,
+      assists: p.stats.assists,
+      saves: p.stats.saves,
+      stuns: p.stats.stuns,
+      ping: p.ping
+    };
+  });
+
+  for (let i = 0; i < 5; i++) {
+    let p = orange_team[i];
+    if (!p) p = { level: '', name: '', point: '', assists: '', assists: '', saves: '', stuns: '', ping: '' };
+    document.getElementById('o-lv-' + i).innerText = p.level;
+    document.getElementById('o-nm-' + i).innerText = p.name;
+    document.getElementById('o-pt-' + i).innerText = p.point;
+    document.getElementById('o-as-' + i).innerText = p.assists;
+    document.getElementById('o-sv-' + i).innerText = p.saves;
+    document.getElementById('o-st-' + i).innerText = p.stuns;
+    document.getElementById('o-pn-' + i).innerText = p.ping;
+  }
+
+  const blue_team = sessionData.teams[0].players.map((p) => {
+    return {
+      level: p.level,
+      name: p.name,
+      point: p.stats.points,
+      assists: p.stats.assists,
+      saves: p.stats.saves,
+      stuns: p.stats.stuns,
+      ping: p.ping
+    };
+  });
+
+  console.log(blue_team);
+
+  for (let i = 0; i < 5; i++) {
+    let p = blue_team[i];
+    if (!p) p = { level: '', name: '', point: '', assists: '', assists: '', saves: '', stuns: '', ping: '' };
+    document.getElementById('b-lv-' + i).innerText = p.level;
+    document.getElementById('b-nm-' + i).innerText = p.name;
+    document.getElementById('b-pt-' + i).innerText = p.point;
+    document.getElementById('b-as-' + i).innerText = p.assists;
+    document.getElementById('b-sv-' + i).innerText = p.saves;
+    document.getElementById('b-st-' + i).innerText = p.stuns;
+    document.getElementById('b-pn-' + i).innerText = p.ping;
+  }
+
+
 }
