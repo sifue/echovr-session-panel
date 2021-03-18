cdf6808e51d8488d05cdc7f03960332cf9cefeba
sifue <yoshimura@soichiro.org>
2021-03-18 21:15:42 +0900

    Implement fetting data

--- preload.js
+++ preload.js
@@ -1,10 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+  setInterval(function(){
+    fetch('http://127.0.0.1:6721/session')
+    .then(response => response.json())
+    .then(data => renderHTML(data));
+  }, 1000);
+
+});
+
+/**
+ * render HTML with session API Data
+ * @param {*} sessionData see https://github.com/Ajedi32/echovr_api_docs
+ */
+function renderHTML(sessionData) {
+
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
   };
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type]);
-  }
-});
+  console.log(sessionData);
+  replaceText('game-clock-display', sessionData.game_clock_display.split('.')[0])
+
+}
